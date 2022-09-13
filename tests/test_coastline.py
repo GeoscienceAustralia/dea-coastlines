6fc1e6a5baa2e82c085a1a9e5e78da2777da0de5
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-09-13 05:21:58 +0000

    merge test

---
+++ tests/test_coastline.py
@@ -0,0 +1,73 @@
+import pytest
+from click.testing import CliRunner
+from coastlines.raster import generate_rasters_cli
+from coastlines.continental import continental_cli
+from coastlines.vector import generate_vectors_cli
+
+
+def test_generate_rasters_cli():
+    runner = CliRunner()
+    result = runner.invoke(
+        generate_rasters_cli,
+        [
+            "--config_path",
+            "configs/dea_coastlines_config.yaml",
+            "--study_area",
+            "1098",
+            "--raster_version",
+            "testing",
+            "--start_year",
+            "2015",
+            "--end_year",
+            "2020",
+        ]
+    )
+    assert result.exit_code == 0
+    assert result.output == ''
+
+
+@pytest.mark.depends(on=['test_generate_rasters_cli'])
+def test_generate_vector_cli():
+    runner = CliRunner()
+    result = runner.invoke(
+        generate_vectors_cli,
+        [
+            "--config_path",
+            "configs/dea_coastlines_config.yaml",
+            "--study_area",
+            "1098",
+            "--raster_version",
+            "testing",
+            "--start_year",
+            "2015",
+            "--end_year",
+            "2020",
+            "--baseline_year",
+            "2020",
+        ]
+    )
+    assert result.exit_code == 0
+    assert result.output == ''
+
+
+
+@pytest.mark.depends(on=['test_generate_vector_cli'])
+def test_generate_continental_cli():
+    runner = CliRunner()
+    result = runner.invoke(
+        continental_cli,
+        [
+            "--vector_version",
+            "testing",
+            "--shorelines",
+            "True",
+            "--ratesofchange",
+            "True",
+            "--hotspots",
+            "[10000, 5000, 1000]",
+            "--baseline_year",
+            "2020",
+        ]
+    )
+    assert result.exit_code == 0
+    assert result.output == ''
