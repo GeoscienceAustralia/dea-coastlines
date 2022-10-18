d6832cf706c1cc099c5047c973ecced02e7ddde6
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2022-10-18 13:42:49 +1100

    Fix bug with incorrect p-value threshold used to identify significant rates of change (#40)

--- coastlines/continental.py
+++ coastlines/continental.py
@@ -48,7 +48,7 @@ def wms_fields(gdf):
             wms_conf=gdf.se_time * 1.96,
             wms_grew=gdf.rate_time < 0,
             wms_retr=gdf.rate_time > 0,
-            wms_sig=gdf.sig_time <= 0,
+            wms_sig=gdf.sig_time <= 0.01,
             wms_good=gdf.certainty == "good",
         )
     )
